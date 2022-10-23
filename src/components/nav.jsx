ab018616aac8c804dea0410ddc2e6c6e5efad67b
Akshat <akshatpandey2501@gmail.com>
2022-10-23 09:58:18 +0530

    navbar added

---
+++ src/components/nav.jsx
@@ -0,0 +1,18 @@
+
+import React from "react";
+
+
+function Nav(){
+    return(
+        <div className="navbar">
+            <p className="appname">SPACES</p>
+            <ul>
+            <li>Home</li>
+            <li>Sign up</li>
+            <li>Login</li>
+            </ul>
+
+        </div>
+    );
+}
+export default Nav;
